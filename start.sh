1248b8188d30c71f69c9d08cfc0b53e4d2f20284
P R I Y O <84788291+itspriyo@users.noreply.github.com>
2021-10-12 18:25:33 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/itspriyo/Music_Player /Music_Player
+  git clone https://github.com/itspriyo/Priyo_Music /Priyo_Music
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/itspriyo/Music_Player -b $BRANCH /Music_Player
+  git clone https://github.com/itspriyo/Priyo_Music -b $BRANCH /Priyo_Music
 fi
 cd /Music_Player
 pip3 install -U -r requirements.txt
